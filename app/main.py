d0cb4f1675ea3b162d7388a54814b6f81595a672
franromano31 <fran.roma1@gmail.com>
2025-11-08 20:54:00 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Vida Sana API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # o ["*"] durante desarrollo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluir todas las rutas
 app.include_router(api_router)
